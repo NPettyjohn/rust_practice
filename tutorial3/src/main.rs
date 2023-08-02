4b6097e91452415fc8e2851bdbbea168f8b60e16
npettyjohn <nicholas.pettyjohn@gmail.com>
2023-08-01 20:17:24 -0700

    finished tutorial3

--- tutorial3/src/main.rs
+++ tutorial3/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 4;
+    println!("x is: {}", x);
+
+    {
+        let x = x - 2;
+        println!("x is: {}", x);
+    }
+
+    let x = x + 1;
+    println!("x is: {}", x);
+
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("there are {} seconds in a minute", SECONDS_IN_MINUTE);
+
 }
